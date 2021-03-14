26f56869d496b5e8b531c1fc9b957e55400f6042
RD-Ricardo <ricardodias169@gmail.com>
2021-03-14 12:22:27 -0300

    Finalizando Programa Cebolinha

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            string frase;
+            Console.WriteLine("Cebolinha");
+            Console.WriteLine("---------");
+            Console.WriteLine("Digite um frase");
+            frase =  Console.ReadLine();
+            frase = frase.Replace("r" , "l" );
+            Console.WriteLine(frase);            
+
+        }
+    }
+}
